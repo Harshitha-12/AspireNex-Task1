46bcf7e82cdae467bfe4081b8ce847b9c17761a5
Harshitha <21wh1a12b0@bvrithyderabad.edu.in>
2024-07-03 17:12:26 +0530

    commiting my personal code

---
+++ app/contact/messages/page.js
@@ -0,0 +1,9 @@
+import MessageContent from "@/components/UI/Contact/message-content";
+
+export const metadata = { title: "M Harshitha - Check Messages 📩", robots: "noindex" };
+
+const Messages = () => {
+  return <MessageContent />;
+};
+
+export default Messages;
---
+++ app/page.js
@@ -0,0 +1,10 @@
+import HomeContent from "../components/UI/Home";
+
+export const metadata = {
+  title: "Manukonda Harshitha - Portfolio",
+  description: "Manukonda Harshitha's portfolio website",
+};
+
+const Home = () => <HomeContent />;
+
+export default Home;
